51ca6c7c1502a2a3e7bcf76164e95b09b3149083
Cristina <acristina4040@gmail.com>
2025-02-08 16:25:47 +0100

    delete unnecesar files, add Routing and pages to the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import MainPage from "./pages/MainPage/MainPage";
+import ShopPage from "./pages/ShopPage/ShopPage";
+import CategoriesPage from "./pages/CategoriesPage/CategoriesPage";
+import CartPage from "./pages/CartPage/CartPage";
+import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
+import CreateNewPasswordPage from "./pages/CreateNewPasswordPage/CreateNewPasswordPage";
+import EmailSentPage from "./pages/EmailSentPage/EmailSentPage";
+import ForgotPaswordPage from "./pages/ForgotPaswordPage/ForgotPaswordPage";
+import LogInPage from "./pages/LogInPage/LogInPage";
+import MyAccountPage from "./pages/MyAccountPage/MyAccountPage";
+import MyAddressesPage from "./pages/MyAddressesPage/MyAddressesPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import OrderHistoryPage from "./pages/OrderHistoryPage/OrderHistoryPage";
+import PaymentPage from "./pages/PaymentPage/PaymentPage";
+import ProductsPage from "./pages/ProductsPage/ProductsPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import SingleProductPage from "./pages/SingleProductPage/SingleProductPage";
+import TermsAndConditionsPage from "./pages/TermsAndConditionsPage/TermsAndConditionsPage";
+import ThankYouPage from "./pages/ThankYouPage/ThankYouPage";
+import WishListPage from "./pages/WishListPage/WishListPage";
+import PrivacyAndPolicyPage from "./pages/PrivacyAndPolicyPage/PrivacyAndPolicyPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/product/:productId" element={<SingleProductPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/thank-you" element={<ThankYouPage />} />
+
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/log-in" element={<LogInPage />} />
+        <Route path="/forgot-password" element={<ForgotPaswordPage />} />
+        <Route path="/email-sent" element={<EmailSentPage />} />
+        <Route path="/ceate-new-paswort" element={<CreateNewPasswordPage />} />
+        <Route path="/my-account" element={<MyAccountPage />} />
+        <Route path="/order-history" element={<OrderHistoryPage />} />
+        <Route path="/wish-list" element={<WishListPage />} />
+        <Route path="/adress" element={<MyAddressesPage />} />
+        <Route path="/terms-and-conditions" element={<TermsAndConditionsPage />} />
+        <Route path="/product/:carCode" element={<PrivacyAndPolicyPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 }

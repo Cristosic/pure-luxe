b9650501f037e38916c5d44f18adb8becbe2edf9
Cristina <acristina4040@gmail.com>
2025-03-09 17:45:17 +0100

    add PromoBanner to MainPage

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,6 +4,7 @@ import WhyChooseUs from "../../components/WhyChooseUs/WhyChooseUs";
 import CategoryContainer from "../../components/CategoryContainer/CategoryContainer";
 import SuperSavings from "../../components/SuperSavings/SuperSavings";
 import NewColletions from "../../components/NewColletions/NewColletions";
+import PromoBanner from "../../components/PromoBanner/PromoBanner";
 
 export default function MainPage() {
   return (
@@ -13,6 +14,7 @@ export default function MainPage() {
       <CategoryContainer />
       <SuperSavings />
       <NewColletions />
+      <PromoBanner />
     </div>
   );
 }
